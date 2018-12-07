f810205053434755f67f02bd965d7d50fd3f1576
Rafael Hernandez <rhernanf@hotmail.com>
2018-12-07 14:34:05 -0600

    revisando campos

--- grails-app/domain/CENTRALSQ/SqtDeudor.groovy
+++ grails-app/domain/CENTRALSQ/SqtDeudor.groovy
@@ -6,12 +6,17 @@ class SqtDeudor {
     Date fechaMod
     int enviado
     String subCuenta
+    int conbinacion
     SqtUsuario usuarioMod
     SqtTipo tipo
     SqtCliente cliente
+    SqtCondicionPago condicionPago
+    SqtEstatus estatus
 
     static constraints = {
-        nombre maxSize: 70, nullable: false
-        rfc maxSize: 13
+        nombre maxSize: 70
+        rfc maxSize: 13, nullable: true
+        subcuenta maxSize: 5, nullable: true
+        usuarioMod nullable: false
     }
 }
