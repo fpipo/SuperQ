995d8553075131028f033bc297bd7e687f0330b8
Rafael Hernandez <rhernanf@hotmail.com>
2018-12-26 12:54:06 -0600

    Se modifico tabla sqtprecioproducto

--- grails-app/domain/CENTRALSQ/SqtPrecioProducto.groovy
+++ grails-app/domain/CENTRALSQ/SqtPrecioProducto.groovy
@@ -3,21 +3,10 @@ package CENTRALSQ
 class SqtPrecioProducto {
     Date fechaInicio
     Date fechaFin
-    Double precioCompraSiva
-    Double montoIvaPrecioCompra
     int aplicaPcompraOferta
     Double pcompraOfertaSiva
     Double mIvaPcompraOferta
-    Double precioVentaNormal
-    Double margenUtilidadNormal
-    Double precioVentaSiva
-    Double montoIvaPrecioVenta
-    Double precioVentaCiva
-    int sugerido
-    Double precioSugerido
-    Double montoIvaPrecioSugerido
-    Double margenUtilidadSugerido
-    String sugeridoPor
+    Double precio
     Double pctVarianza
     Date fechaMod
     int enviado
@@ -30,17 +19,8 @@ class SqtPrecioProducto {
 
     static constraints = {
         fechaFin nullable: true
-        precioCompraSiva nullable: true
-        montoIvaPrecioCompra nullable: true
         pcompraOfertaSiva nullable: true
         mIvaPcompraOferta nullable: true
-        precioVentaNormal nullable: true
-        margenUtilidadNormal nullable: true
-        sugerido nullable: true
-        precioSugerido nullable: true
-        montoIvaPrecioSugerido nullable: true
-        margenUtilidadSugerido nullable: true
-        sugeridoPor nullable: true
         pctVarianza nullable: true
         montoIsb nullable: true
         enviado nullable:true
