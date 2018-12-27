6ae4dacfc61bbfc0c21b9d092d6b474671ea366c
Rafael Hernandez <rhernanf@hotmail.com>
2018-12-27 14:38:52 -0600

    Se adiciono campo clave a tabla sqt_area

--- grails-app/domain/CENTRALSQ/SqtArea.groovy
+++ grails-app/domain/CENTRALSQ/SqtArea.groovy
@@ -1,11 +1,13 @@
 package CENTRALSQ
 
 class SqtArea {
+    String clave
     String descripcion
     String centroCostos
     int enviado
 
     static constraints = {
+        clave maxSize: 3
         descripcion maxSize: 50, nullable: true
         centroCostos maxSize: 30, nullable: true
         enviado nullable: true
